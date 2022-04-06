69d35f9a11d788c9c5a6a5222b30fd2f3300c2f2
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-04-06 18:44:36 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+  function handleBlur (){
+    console.log('Hey! Eyes on me!')
+  }
+  function handleFocus(){
+    console.log("Good!")
+  }
+  return (<button onBlur={handleBlur} onFocus={handleFocus}>Eyes on me</button>)
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad() {
+ function password(){
+  return console.log("Entering password...")
+ } 
+  
+
+  return <input type="password" onChange={(password)} />;
+}
+
+export default Keypad;
